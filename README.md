f05a0e5983fab8140f4035aea89d77660e4dd748
Marina Sundström <marna.li@outlook.com>
2025-03-03 23:02:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CSS and Component Library
+
+Styles created in style of Bootstrap 5, with help from ChatGPT.
+
+```
+npm run sass
+```
+
+Nav menu from [W3 Schools](https://www.w3schools.com/howto/).
---
+++ script.js
@@ -0,0 +1,35 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function initScrollToTop() {
+    var scrollToTop = document.querySelector("#scrollToTop");
+
+    if (!scrollToTop) return;
+
+    scrollToTop.addEventListener("click", ev => {
+        ev.preventDefault();
+
+        document.documentElement.scrollTo(0, 0);
+    });
+}
